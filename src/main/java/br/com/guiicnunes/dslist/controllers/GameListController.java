3c418076ab310a6d6c70f6ebea767f32924cef5a
Guilherme Nunes <guiicnunes@gmail.com>
2024-11-13 19:05:31 -0300

    creates new endpoint to get games from list

--- src/main/java/br/com/guiicnunes/dslist/controllers/GameListController.java
+++ src/main/java/br/com/guiicnunes/dslist/controllers/GameListController.java
@@ -1,9 +1,12 @@
 package br.com.guiicnunes.dslist.controllers;
 
 import br.com.guiicnunes.dslist.dto.GameListDTO;
+import br.com.guiicnunes.dslist.dto.GameMinDTO;
 import br.com.guiicnunes.dslist.services.GameListService;
+import br.com.guiicnunes.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +19,17 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll() {
         var result = gameListService.findAll();
+        return result;
+    }
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId) {
+        var result = gameService.findByList(listId);
         return result;
     }
 }
