e5db89495493f701d2454f1ea3fc7c2f6bf74081
Guilherme Nunes <guiicnunes@gmail.com>
2024-11-13 18:35:36 -0300

    creates new endpoint to find game by id

--- src/main/java/br/com/guiicnunes/dslist/controllers/GameController.java
+++ src/main/java/br/com/guiicnunes/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package br.com.guiicnunes.dslist.controllers;
 
+import br.com.guiicnunes.dslist.dto.GameDTO;
 import br.com.guiicnunes.dslist.dto.GameMinDTO;
 import br.com.guiicnunes.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        var result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
