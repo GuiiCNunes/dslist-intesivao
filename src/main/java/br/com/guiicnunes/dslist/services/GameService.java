7e8f6b9ef0425b9b57e9675939d23f5ae75ba8e4
Guilherme Nunes <guiicnunes@gmail.com>
2024-11-13 18:35:15 -0300

    updates service to find a game by id

--- src/main/java/br/com/guiicnunes/dslist/services/GameService.java
+++ src/main/java/br/com/guiicnunes/dslist/services/GameService.java
@@ -1,9 +1,12 @@
 package br.com.guiicnunes.dslist.services;
 
+import br.com.guiicnunes.dslist.dto.GameDTO;
 import br.com.guiicnunes.dslist.dto.GameMinDTO;
+import br.com.guiicnunes.dslist.entities.Game;
 import br.com.guiicnunes.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
        var result = gameRepository.findAll();
        List<GameMinDTO> dto = result.stream().map(GameMinDTO::new).toList();
