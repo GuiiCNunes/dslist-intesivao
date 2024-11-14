3e5f8a754b6f4c7d394cf7f48be85b78ab75045e
Guilherme Nunes <guiicnunes@gmail.com>
2024-11-14 10:49:16 -0300

    inserts new endpoint to replace game position on list

--- src/main/java/br/com/guiicnunes/dslist/controllers/GameListController.java
+++ src/main/java/br/com/guiicnunes/dslist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package br.com.guiicnunes.dslist.controllers;
 
 import br.com.guiicnunes.dslist.dto.GameListDTO;
 import br.com.guiicnunes.dslist.dto.GameMinDTO;
+import br.com.guiicnunes.dslist.dto.ReplacementDTO;
 import br.com.guiicnunes.dslist.services.GameListService;
 import br.com.guiicnunes.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,9 +25,15 @@ public class GameListController {
         var result = gameListService.findAll();
         return result;
     }
+
     @GetMapping(value = "/{listId}/games")
     public List<GameMinDTO> findByList(@PathVariable Long listId) {
         var result = gameService.findByList(listId);
         return result;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
