8bea949812ed64b4454f48d513af2637561a4386
Guilherme Nunes <guiicnunes@gmail.com>
2024-11-14 10:48:36 -0300

    inserts service to replace game position on list

--- src/main/java/br/com/guiicnunes/dslist/services/GameListService.java
+++ src/main/java/br/com/guiicnunes/dslist/services/GameListService.java
@@ -1,7 +1,9 @@
 package br.com.guiicnunes.dslist.services;
 
 import br.com.guiicnunes.dslist.dto.GameListDTO;
+import br.com.guiicnunes.dslist.projections.GameMinProjection;
 import br.com.guiicnunes.dslist.repositories.GameListRepository;
+import br.com.guiicnunes.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,9 +16,27 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
        var result = gameListRepository.findAll();
        return result.stream().map(GameListDTO::new).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        var removed = list.remove(sourceIndex);
+        list.add(destinationIndex, removed);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
